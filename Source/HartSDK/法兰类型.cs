7be4edb9c34b6f1fb480b6352bcb7c94b46f0d30
233955501 <233955501@qq.com>
2015-01-22 20:46:24 +0800

    1

---
+++ Source/HartSDK/法兰类型.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HartSDK
+{
+    public enum  法兰类型
+    {
+        常用=0x0C,
+        Coplanar=0x0D,
+        远传膜盒密封=0x0E,
+    }
+}
